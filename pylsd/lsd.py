aec4a0680ab630f4f1b9d01abf75d6d9bb117e29
vestri <vestri@gmail.com>
2016-07-02 17:44:33 +0200

    put temp created file. It works with Python 2.7. BUG: temp file name is 'D' with python 3.5, need to find why

--- pylsd/lsd.py
+++ pylsd/lsd.py
@@ -19,10 +19,10 @@ def lsd(src):
     src = (ctypes.c_double * lens)(*src)
     lsdlib.lsdGet(src, ctypes.c_int(rows), ctypes.c_int(cols), temp)
 
-    fp = open('D', 'r')
+    fp = open(temp, 'r')
     cnt = fp.read().strip().split(' ')
     fp.close()
-    os.remove('D')
+    os.remove(temp)
 
     count = int(cnt[0])
     dim = int(cnt[1])
